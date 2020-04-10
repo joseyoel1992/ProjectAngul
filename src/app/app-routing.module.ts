e90dda7fba86d3cb429a808fd8d09e307bc60369
Yoel <joseyoelmalpica37@gmail.com>
2020-04-11 00:01:00 +0200

    He configurado las rutas y agregado useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+
+
+
+const routes: Routes = [
+{path: '' , component: PortafolioComponent},
+{path: 'item' , component: ItemComponent},
+{path: 'about' , component: AboutComponent},
+{path: '**' , component: PortafolioComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
