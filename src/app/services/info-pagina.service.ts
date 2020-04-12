d0a2c200f3a2a4121cfe179453c88cee59df9e36
Yoel <joseyoelmalpica37@gmail.com>
2020-04-12 02:04:28 +0200

    He agregado los datos de about en la base de datos

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,15 +8,29 @@ import { infoPaginaInterface } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   public info: infoPaginaInterface={};
+  public equipo:any;
 
   constructor(private http: HttpClient) {
-    // console.log('Hola desde el servicio');
+
+  this.cargarInfo();
+  this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json').subscribe(
       (result: infoPaginaInterface) => {
         this.info=result;
-        console.log(result.instagram);
       }
     );
-
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-c9b39.firebaseio.com/equipo.json').subscribe(
+      (result: any) => {
+        this.equipo=result;
+        console.log(result);
+      }
+    );
+  }
+
 }
